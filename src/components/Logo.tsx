27bf570941d08e998816d295439b9edd8c7b422e
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-07-06 00:22:02 -0300

    feat: Responsive Subscribe page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,9 @@
-export function Logo() {
+import { HTMLAttributes } from 'react';
+
+export function Logo({ className }: HTMLAttributes<SVGSVGElement>) {
   return (
     <svg
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       width="237"
       height="34"
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -26,19 +26,19 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
-          <Logo />
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto sm:px-6 xl:px-0 gap-4 xl:gap-0 flex-col sm:flex-row">
+        <div className="max-w-[640px] px-6 sm:px-0 text-center sm:text-left">
+          <Logo className="mx-auto sm:m-0" />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
+            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React&nbsp;JS</strong>
           </h1>
           <p className="mt-4 text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 bg-gray-700 border-y border-gray-500 w-full sm:rounded sm:max-w-sm sm:border">
           <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form
